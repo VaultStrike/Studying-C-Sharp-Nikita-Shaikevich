d14dd704aad9253d3ec4ad7ebc15b0b6f46ce4ef
Nick Nick <piratescreen@gmail.com>
2024-05-31 10:08:04 +0300

    Conflict 3

--- Studying-C-Sharp-Nikita-Shaikevich/Conflicts/ConflitFileTest.cs
+++ Studying-C-Sharp-Nikita-Shaikevich/Conflicts/ConflitFileTest.cs
@@ -8,8 +8,9 @@ namespace Studying_C_Sharp_Nikita_Shaikevich.Conflicts
 {
     internal class ConflitFileTest
     {
-        public static void Conflict1()
+        public static void Conflict3()//Changed To Conflict 3 From Conflict 2
         {
+            Console.WriteLine("New Conflict 3!");
             Console.WriteLine("Lets Test This Conflict!");
         }
     }
