be7da3eb53eae887850019462c533aa0f8badade
Nick Nick <piratescreen@gmail.com>
2024-05-31 09:53:53 +0300

    Main -> New Folder (Conflict) -> NewClass  (ConflictFile)

---
+++ Studying-C-Sharp-Nikita-Shaikevich/Conflicts/ConflitFileTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Studying_C_Sharp_Nikita_Shaikevich.Conflicts
+{
+    internal class ConflitFileTest
+    {
+        public static void Conflict1()
+        {
+            Console.WriteLine("Lets Test This Conflict!");
+        }
+    }
+}
