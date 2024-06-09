ee5dab434df7a4b97d500b6ea5e83f6d7721bf4f
Nick Nick <piratescreen@gmail.com>
2024-06-09 19:24:02 +0300

    updated Task

--- Studying-C-Sharp-Nikita-Shaikevich/Program.cs
+++ Studying-C-Sharp-Nikita-Shaikevich/Program.cs
@@ -1,11 +1,17 @@
 ﻿using Studying_C_Sharp_Nikita_Shaikevich;
+using Studying_C_Sharp_Nikita_Shaikevich.UpdatedTask;
 public class Program
 {
     public static void Main(string[] args)
     {
         //runObjects.DemoPcRun();
 
-        ThisObjects.Demo();
+        //ThisObjects.Demo();
+
+        Level_1.RunMe();
+
+
+
 
     }
 }
---
+++ Studying-C-Sharp-Nikita-Shaikevich/UpdatedTask/Level_3_2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Studying_C_Sharp_Nikita_Shaikevich.UpdatedTask
+{
+    public class Level_3_2
+    {
+        public static void RunMe()
+        {
+            Console.WriteLine("Hi, I am class Level3_2.");
+        }
+    }
+}
+
