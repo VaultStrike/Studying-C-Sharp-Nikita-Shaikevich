1db73c2aa8cc3f29f8f39936b35f18144b6fabc9
Nick Nick <piratescreen@gmail.com>
2024-07-19 11:36:49 +0300

    push and secure!

---
+++ Studying-C-Sharp-Nikita-Shaikevich/Inheritance/Electric_Products.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Studying_C_Sharp_Nikita_Shaikevich.Inheritance
+{
+    public class Electric_Products
+    {
+        protected int price;
+        private int id = 0;
+        protected int warrantyPeriod;//in Mothnts
+        private bool isEnergyEfficient;
+
+        public Electric_Products(int price, int warrantyPeriod, bool isEnergyEfficient)
+        {
+            this.price = price;
+            this.warrantyPeriod = warrantyPeriod;
+            this.isEnergyEfficient = isEnergyEfficient;
+            id++;
+        }
+        public virtual void DisplayProduct()
+        {
+            Console.WriteLine($"Id: {id} | Price: {price} | warranty Period in Months: {warrantyPeriod} | EnergyEfficient: {isEnergyEfficient}");
+        }
+    }
+
+
+}
---
+++ Studying-C-Sharp-Nikita-Shaikevich/Practice/Project_Euler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Studying_C_Sharp_Nikita_Shaikevich.Practice
+{
+    internal class Project_Euler
+    {
+        public static void demo()
+        {
+
+        }
+        public static void Solve3()
+        {
+
+        }
+
+
+
+    }
+
+}
