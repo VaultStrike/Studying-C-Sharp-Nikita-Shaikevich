8dd7bf21e5d851ac5aa8d2c8379fe576cdf71657
Nick Nick <piratescreen@gmail.com>
2024-09-27 09:38:04 +0300

    updated

---
+++ Studying-C-Sharp-Nikita-Shaikevich/Design_Pattern/Singleton/GameManager/GameManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Studying_C_Sharp_Nikita_Shaikevich.Design_Pattern.Singleton.GameManager
+{
+    public class GameManager
+    {
+    }
+}
