cfddd65b5ef68adda7e96a93b60f044053dc4340
Nick Nick <piratescreen@gmail.com>
2024-11-24 19:05:05 +0200

    finish summer

---
+++ Studying-C-Sharp-Nikita-Shaikevich/SummerTask.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Studying_C_Sharp_Nikita_Shaikevich
+{
+    public class SummerTask
+    {
+       
+    }
+}
